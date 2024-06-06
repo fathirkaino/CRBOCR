9f5ab8803451b6638e9482a65b1ac17517401f99
Mohamed Fathir <152964920+fathirkaino@users.noreply.github.com>
2024-06-07 03:03:28 +0530

    a

--- CR_BOOK8.py
+++ CR_BOOK8.py
@@ -20,13 +20,6 @@ import random
 import re
 import logging
 
-# Initialize the OCR model
-ocr = PaddleOCR(
-    det_model_dir='/root/.paddleocr/whl/det/en/en_PP-OCRv3_det_infer',
-    rec_model_dir='/root/.paddleocr/whl/rec/en/en_PP-OCRv4_rec_infer',
-    cls_model_dir='/root/.paddleocr/whl/cls/ch_ppocr_mobile_v2.0_cls_infer'
-)
-
 
 # Initialize OCR
 #ocr = PaddleOCR(lang='en', use_gpu=False)
